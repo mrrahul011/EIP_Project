55f0eafa3ff92819738e412a96ab35deb19b8b46
Rahul M R <mrrahul011@gmail.com>
2018-04-30 00:33:09 +0530

    Session 2

---
+++ random_num_gen.py
@@ -0,0 +1,27 @@
+import random
+print("Enter the number of rows: ")
+m = int(input())
+print("Enter the number of column: ")
+n = int(input())
+
+
+for i in range(0,m):
+    for j in range(0,n):
+        print(matrix[i][j],end=" ")
+    print('\n')
+        
+#%%        
+        num = round(random.random(),4)
+        row.append(num)
+    matrix.append(row)
+   
+for i in range(m):
+    for j in range(n):
+        print(matrix[i][j],end=" ")
+    print('\n')
+        
+        
+        
+        
+        
+        
